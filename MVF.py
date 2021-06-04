f53807190baa251d58063dce29e701273c0530a5
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-06-04 11:04:14 +0800

    Update MVF.py

--- MVF.py
+++ MVF.py
@@ -15,7 +15,8 @@ st.sidebar.header('Welcome！ o(*￣▽￣*)ブ')
 author = "Miraka"
 RISKY_ASSETS = get_list()
 N_PORTFOLIOS =  get_portf_num()
-RF = float(st.sidebar.text_input("Input Annualized risk-free rate","1.5%"))
+RF = st.sidebar.text_input("Input Annualized risk-free rate","1.5%")
+RF = float(RF)
 st.sidebar.write("Default annualized risk-free rate: 1.5%")
 N_DAYS = 252
 RISKY_ASSETS.sort()
