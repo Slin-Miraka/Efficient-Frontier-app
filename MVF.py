43096858108e98c5916e259a00b84dcd4a001e29
Slin-Miraka <76611803+Slin-Miraka@users.noreply.github.com>
2021-06-07 11:07:09 +0800

    Update MVF.py

--- MVF.py
+++ MVF.py
@@ -30,9 +30,6 @@ n_assets = len(RISKY_ASSETS)
 st.title("Efficient Frontier APP")
 st.write("Edit by: ",author)
 
-col1, col2 = st.beta_columns(2)
-if col1.button("Clean the portfolio list"):
-  RISKY_ASSETS = []
   
 st.write("**Current components of the portfolio**",RISKY_ASSETS)
 
